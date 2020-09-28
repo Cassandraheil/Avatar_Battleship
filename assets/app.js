6a616ef8f12c4c9fcc2196e61c6ecc8271679995
Cassandra Heil <Cassandraheil19@gmail.com>
2020-09-28 13:37:23 -0400

    objects defined and pop up on screen

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,20 @@
-var aang = "Aang"
-var sokka = "Sokka"
-var katara  = "Katara"
-var toph = "Toph"
-var ozai = "Ozai"
-var azula = "Azula"
-var tiLee = "Ti Lee"
-var mai = "Mai"
+window.onload = function(){
+var aang = {name:"Aang", alignment: "hero", pic: "n/a"};
+var sokka = {name:"Sokka", alignment:"hero"};
+var katara  = {name:"Katara", alignment: "hero"};
+var toph = {name:"Toph", alignment:"hero", pic: "n/a"};
+var ozai = {name:"Ozai", alignment:"villian"};
+var azula = {name:"Azula", alignment: "villian"};
+var tiLee = {name:"Ti Lee", alignment:"villian"};
+var mai = {name:"Mai", alignment:"villian"};
 
 
-var hero = [aang, sokka, katara, toph]
-var villian = [ozai, azula, tiLee, mai]
-console.log(hero)
+var fighters = [aang, sokka, katara, toph, ozai, azula, tiLee, mai]
 
-hero.forEach(element =>
-    console.log(element)
-    // $("#gameArea").append(<div>Element<div>),
-    )
+fighters.forEach((element) => {
+    console.log(element),
+    $("#gameArea").append("<div class='fighter'>",element.name,"</div>")
+});
+
+
+}
