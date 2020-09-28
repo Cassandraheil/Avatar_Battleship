115ddd2906f2b506e9cbb0783942023a2f104e2b
Cassandra Heil <Cassandraheil19@gmail.com>
2020-09-28 12:13:04 -0400

    javascript and css page connected,varibles defined

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+var aang = "Aang"
+var sokka = "Sokka"
+var katara  = "Katara"
+var toph = "Toph"
+var ozai = "Ozai"
+var azula = "Azula"
+var tiLee = "Ti Lee"
+var mai = "Mai"
+
+
+var hero = [aang, sokka, katara, toph]
+var villian = [ozai, azula, tiLee, mai]
+console.log(hero)
+
+hero.forEach(element =>
+    console.log(element)
+    // $("#gameArea").append(<div>Element<div>),
+    )
