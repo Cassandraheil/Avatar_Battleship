4bba80febab58e19592bb4047f7ee7a578b0a8a5
Cassandra Heil <Cassandraheil19@gmail.com>
2020-09-29 17:15:06 -0400

    have a place for images to show up

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-var aang = {name:"Aang", alignment: "hero", pic: "n/a"};
+var aang = {name:"Aang", alignment: "hero", pic: "https://i.insider.com/5ebdbc8c3ad8612a1c7aa143?width=856&format=jpeg"};
 var sokka = {name:"Sokka", alignment:"hero"};
 var katara  = {name:"Katara", alignment: "hero"};
 var toph = {name:"Toph", alignment:"hero", pic: "n/a"};
@@ -13,9 +13,14 @@ var fighters = [aang, sokka, katara, toph, ozai, azula, tiLee, mai]
 
 fighters.forEach((element) => {
     console.log(element),
-    $("#gameArea").append("<div class='fighter'>",element.name,"</div>")
-});
+    $("#gameArea").append("<h3 class='fighter'>",element.name,"</h3>")
+    $("#gameArea").append("<img src="+element.pic+" alt='charater in Avatar' height='200' width='200'>")
+}); 
 
 
 
 }
+
+$(".fighter").click(function(){
+    console.log("this was clicked")
+})
